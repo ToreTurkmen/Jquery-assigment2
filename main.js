0b3eee441fa7ae79ac78decd3425db4e77bd7319
ToreTurkmen <your.email@yourmailserver.com>
2022-10-26 02:12:18 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+$("#image1").click(function(){
+  // Change src attribute of image
+  $(this).attr("src", "../Assignment 2 from Turing to Gates/images/1b.jpg")});
+
+$("#image2").click(function(){
+    // Change src attribute of image
+  $(this).attr("src", "../Assignment 2 from Turing to Gates/images/2b.jpg")});
+
+$("#image3").click(function(){
+      // Change src attribute of image
+$(this).attr("src", "../Assignment 2 from Turing to Gates/images/3b.jpg")}); 
